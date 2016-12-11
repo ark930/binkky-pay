2b9080b0a2a3354dbdb111e7e8169a8c57582ca5
edwin <lark930@gmail.com>
2016-12-11 16:17:25 +0800

    跟换ChannelAlipay中的表名

--- app/Models/ChannelAlipay.php
+++ app/Models/ChannelAlipay.php
@@ -8,7 +8,7 @@ class ChannelAlipay extends Model
 {
     public static function getPaymentParameters()
     {
-        return app('db')->table('alipays')
+        return app('db')->table('channel_alipays')
             ->select('appid', 'private_key', 'alipay_public_key')
             ->first();
     }
