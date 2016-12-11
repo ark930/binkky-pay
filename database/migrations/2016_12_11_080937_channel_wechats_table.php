6fe262ecb6e6f3b6ed245f666ac44f229e60a57e
edwin <lark930@gmail.com>
2016-12-11 17:21:14 +0800

    微信支付建表

---
+++ database/migrations/2016_12_11_080937_channel_wechats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChannelWechatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channel_wechats', function(Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('merchant_id');
+            $table->char('appid', 18)->comment('公众账号ID');
+            $table->string('mch_id', 32)->comment('商户号');
+            $table->char('key', 32)->comment('商户支付密钥');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel_wechats');
+    }
+}
