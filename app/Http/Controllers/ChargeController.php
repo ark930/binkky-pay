8c64f5e8fc2f347c70cca85ce7d9f3930a8d5f24
edwin <lark930@gmail.com>
2016-12-15 22:58:45 +0800

    update ChargeController

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -2,20 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\Channel\Payment;
+use App\Models\Charge;
+use Illuminate\Http\Request;
+
 class ChargeController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
-        return response('', 200);
+        $charge = new Charge();
+        $charge['channel'] = $request->input('channel');
+        $charge['type'] = $request->input('type');
+        $charge['order_no'] = $request->input('order_no');
+        $charge['amount'] = $request->input('amount');
+        $charge['currency'] = $request->input('currency');
+        $charge->save();
+
+        $payment = Payment::make('alipay_wap');
+        $payUrl = $payment->charge($charge);
+
+        return response([
+            'credential' => $payUrl
+        ], 200);
     }
 
-    public function retrieve()
+    public function query($charge_id)
     {
-        return response('', 200);
+        $charge = Charge::findOrFail($charge_id);
+        $channel = $charge['channel'];
+        $status = $charge['status'];
+        if($status === 'finish' || $status === 'close') {
+            return $charge;
+        }
+
+        $payment = Payment::make($channel);
+        $data = $payment->query($charge);
+
+        return response($data, 200);
     }
 
-    public function notify()
+    public function notify(Request $request, $charge_id)
     {
-        return response('', 200);
+        $charge = Charge::findOrFail($charge_id);
+        $channel = $charge['channel'];
+        $status = $charge['status'];
+        if($status === 'finish' || $status === 'close') {
+            return response('success', 200);
+        }
+
+        $notify = $request->all();
+        $payment = Payment::make($channel);
+        $data = $payment->notify($charge, $notify);
+
+        return response('success', 200);
     }
 }
