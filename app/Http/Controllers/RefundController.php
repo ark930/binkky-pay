4f574db4dea5ca43e384441440c03573d8f584e9
edwin <lark930@gmail.com>
2016-12-15 22:58:13 +0800

    update RefundController

--- app/Http/Controllers/RefundController.php
+++ app/Http/Controllers/RefundController.php
@@ -2,15 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\Channel\Payment;
+use App\Models\Charge;
+use App\Models\Refund;
+
 class RefundController extends Controller
 {
     public function create()
     {
-        return response('', 200);
+        $charge_id = 1;
+        $charge = Charge::findOrFail($charge_id);
+
+        $refund = new Refund();
+        $refund['order_no'] = str_random(16);
+        $refund['amount'] = 1;
+        $refund['currency'] = 'cny';
+
+        $channel = $charge['channel'];
+        $status = $charge['status'];
+        if($status === 'finish' || $status === 'close') {
+            return $charge;
+        }
+
+        $payment = Payment::make($channel);
+        $data = $payment->refund($charge, $refund);
+
+        return $data;
     }
 
-    public function query()
+    public function query($refund_id)
     {
-        return response('', 200);
+        $refund = Refund::findOrFail($refund_id);
+        $charge = $refund->charge;
+
+        $channel = $charge['channel'];
+        $status = $charge['status'];
+        if($status === 'finish' || $status === 'close') {
+            return $charge;
+        }
+
+        $payment = Payment::make($channel);
+        $data = $payment->refundQuery($charge, $refund);
+
+        return response($data, 200);
     }
 }
