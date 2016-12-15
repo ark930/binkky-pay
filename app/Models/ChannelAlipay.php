41af7fa5b83736b58085d7e3da78599c6177dbbd
edwin <lark930@gmail.com>
2016-12-16 00:48:11 +0800

    将支付宝支付参数存入缓存

--- app/Models/ChannelAlipay.php
+++ app/Models/ChannelAlipay.php
@@ -3,13 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Redis;
 
 class ChannelAlipay extends Model
 {
     public static function getPaymentParameters()
     {
-        return app('db')->table('channel_alipays')
-            ->select('appid', 'private_key', 'alipay_public_key')
-            ->first();
+        $data = Redis::command('HGETALL', ['alipay']);
+        if(empty($data)) {
+            $data = app('db')->table('channel_alipays')
+                ->select('appid', 'private_key', 'alipay_public_key')
+                ->first();
+
+            $data = collect($data)->toArray();
+
+            $hashData = ['alipay'];
+            foreach ($data as $k => $v) {
+                $hashData[] = $k;
+                $hashData[] = $v;
+            }
+            Redis::command('HMSET', $hashData);
+        }
+
+        return $data;
     }
 }
