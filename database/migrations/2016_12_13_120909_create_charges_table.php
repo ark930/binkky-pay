7da4fe0ec3ccfe7a33fc7297e2daab56b5d53530
edwin <lark930@gmail.com>
2016-12-28 12:08:50 +0800

    修改 charges 表

--- database/migrations/2016_12_13_120909_create_charges_table.php
+++ database/migrations/2016_12_13_120909_create_charges_table.php
@@ -18,20 +18,20 @@ class CreateChargesTable extends Migration
             $table->unsignedInteger('app_id');
             $table->enum('channel', ['alipay', 'wechat'])->comment('渠道名称');
             $table->enum('type', ['qr', 'scan', 'pub', 'wap'])->comment('支付类型');
-            $table->string('order_no', 64)->comment('商户订单号');
-            $table->string('transaction_no', 64)->nullalbe()->comment('渠道交易单号');
+            $table->string('trade_no', 64)->comment('商户订单号');
+            $table->string('tn', 64)->nullable()->comment('渠道交易单号, Transaction Number');
             $table->unsignedInteger('amount')->comment('支付金额，以分为单位');
             $table->unsignedInteger('amount_refunded')->default(0)->comment('退款金额，以分为单位');
             $table->unsignedInteger('amount_settled')->comment('清算金额，以分为单位');
 //            $table->boolean('refunded')->default(false)->comment('是否有退款，包括全额退款和部分退款');
-            $table->string('subject', 64)->comment('支付标题');
-            $table->string('body', 128)->comment('支付描述');
+            $table->string('title', 64)->comment('支付标题');
+            $table->string('desc', 128)->comment('支付描述');
             $table->enum('currency', ['cny'])->comment('货币类型');
-            $table->enum('status', ['pending', 'succeeded', 'failed', 'closed'])->comment('支付状态');
             $table->string('client_ip', 15)->comment('客户端IP');
+            $table->string('auth_code')->nullable()->comment('刷卡支付用户支付授权码');
+            $table->enum('status', ['pending', 'succeeded', 'failed', 'closed'])->comment('支付状态');
             $table->timestamp('paid_at')->nullable()->comment('支付完成时间');
             $table->timestamp('expired_at')->nullable()->comment('支付过期时间');
-            $table->string('auth_code')->nullable()->comment('刷卡支付用户支付授权码');
             $table->timestamps();
         });
     }
