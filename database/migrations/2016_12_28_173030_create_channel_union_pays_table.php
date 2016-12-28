ecb442323f90325ea7a8b8db8dd9e9a0c449f13b
edwin <lark930@gmail.com>
2016-12-28 18:37:47 +0800

    新增channel_union_pays表

---
+++ database/migrations/2016_12_28_173030_create_channel_union_pays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChannelUnionPaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channel_union_pays', function(Blueprint $table) {
+            $table->increments('id');
+            $table->char('mer_id', 15)->comment('银联商户号');
+            $table->string('cert_id', 64)->comment('银联用户证书序列号');
+            $table->text('cert')->comment('银联用户证书');
+            $table->string('cert_password', 64)->nullable()->comment('银联用户证书密码');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channel_union_pays');
+    }
+}
