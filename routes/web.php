d78f35ec4577a06ef62bd429636e5559bb4715cd
edwin <lark930@gmail.com>
2017-01-16 13:45:00 +0800

    add comments to web.php

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,27 @@
 $app->get('/', 'ExampleController@test');
 
 $app->group(['middleware' => 'auth', 'prefix' => 'v1'], function() use ($app) {
+    // 支付接口
     $app->post('charges', 'ChargeController@create');
+
+    // 支付查询接口
     $app->get('charges/{charge_id}', 'ChargeController@query');
+
+    // 关闭支付接口
 //    $app->get('charges/{charge_id}/close', 'ChargeController@close');
+
+    // 支付异步通知接口
     $app->get('charges/{charge_id}/notify', ['as' => 'notify', 'uses' => 'ChargeController@notify']);
     $app->post('charges/{charge_id}/notify', 'ChargeController@notify');
 
+    // 退款接口
     $app->post('refunds', 'RefundController@create');
     $app->get('refunds/{refund_id}', 'RefundController@query');
 
+    // 对账接口
 //    $app->get('charges/bill', 'ChargeController@bill');
 
+    // 支付渠道参数接口
     $app->group(['prefix' => 'channels'], function() use($app) {
         $app->post('alipay', 'Channels\AlipayController@store');
         $app->get('alipay', 'Channels\AlipayController@show');
@@ -32,15 +42,13 @@ $app->group(['middleware' => 'auth', 'prefix' => 'v1'], function() use ($app) {
         $app->get('wechat', 'Channels\WechatController@show');
     });
 
-    $app->group(['prefix' => 'keys'], function() use($app) {
-        $app->post('/', 'KeyController@store');
-        $app->get('/{partner_id}', 'KeyController@show');
-        $app->put('/{partner_id}', 'KeyController@update');
-    });
+//    $app->group(['prefix' => 'keys'], function() use($app) {
+//        $app->post('/', 'KeyController@store');
+//        $app->get('/{partner_id}', 'KeyController@show');
+//        $app->put('/{partner_id}', 'KeyController@update');
+//    });
 });
 
-$app->options('/alipay_wap', 'ExampleController@alipayWap');
 $app->get('/alipay_wap', 'ExampleController@alipayWap');
 
-$app->options('/wechat_qr', 'ExampleController@wechatQR');
 $app->get('/wechat_qr', 'ExampleController@wechatQR');
